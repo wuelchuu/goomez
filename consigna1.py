89dbfe5753e6eb3eb8c4820e32590bdd26747854
wuelchuu <168199306+wuelchuu@users.noreply.github.com>
2024-04-26 11:50:33 -0300

    Add files via upload

---
+++ consigna1.py
@@ -0,0 +1,11 @@
+nota= int(input("ingrese numero 1"))
+nota2=int(input("ingrese numero 2"))
+nota3=int(input("ingrese numero 3"))
+prom= (nota+nota2+nota3)/3
+if prom >=7:
+    print("cheeto aprobaste")
+
+if prom>=4:
+    print("regular")
+else: 
+    print("desaprobado")
